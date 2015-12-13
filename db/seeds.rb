c05d05efa2bee1d8252220607961fc2bffd14a60
racheltstevens <racheltstevens@gmail.com>
2015-12-12 19:32:08 -0500

    Seeds properly

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,12 +8,19 @@
 
 users = Array.new
 
+
 %w(rts srt rst str).each do |name|
   email = "#{name}@#{name}.com"
-  next if User.exists? email: email
-  id = User.create!(email: email, password: 'abc123',
-               password_confirmation: 'abc123')[:id]
+  id = User.find_by!(email: email)
+
+  if id == nil
+    id = User.create!(email: email, password: 'abc123',
+                 password_confirmation: 'abc123')[:id]
+  end
+  users.push(id)
 end
+
+p users
 
 lat_lngs = [[42.360082, -71.058880, "Boston"],[41.823989, -71.412834, "Providence"],[43.161030, -77.610922, "Rochester"], [40.712784, -74.005941, "New York City"], [42.262593, -71.802293, "Worcester"]] 
 
@@ -28,8 +35,8 @@ ride1 = Ride.create!(owner: User.find_by(email: "rts@rts.com"), destination: loc
 
 ride2 = Ride.create!(owner: User.find_by(email: "rts@rts.com"), destination: locations[0], start_point: locations[1], departure_date_time: "Today", spots_left: 3)[:id]
 
-RidePassenger.create!(ride_id: ride1, user_id: users[2])
-RidePassenger.create!(ride_id: ride2, user_id: users[3])
+RidePassenger.create!(ride_id: ride1, user_id: 2)
+RidePassenger.create!(ride_id: ride2, user_id: 3)
 
 
 
