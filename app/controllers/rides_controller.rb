f396a5c200eeba2419e73c244abfd7966b9be04e
racheltstevens <racheltstevens@gmail.com>
2015-12-13 12:19:53 -0500

    Adds associations to User

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -4,34 +4,61 @@ class RidesController < OpenReadController
   end
 
   def show
-    render json: Ride.where(params[:id])
+    @ride = Ride.find_by_id(params[:id])
+    render json: @ride, status: 200
   end
 
   def create
-    @ride = current_user.rides.new(ride_params)
+    begin
+     Ride.transaction do
+      destination = location.find_or_create_by!(address: ride_params[:address], lat: ride_params[:destination][:lat], lng:ride_params[:destination][:lng])
+      start_point = location.find_or_create_by!(address: ride_params[:address], lat: ride_params[:destination][:lat], lng:ride_params[:destination][:lng])
 
-    if @ride.save
-      render json: @ride, status: :created, location: @book
-    else
-      render json: @book.errors, status: :unprocessable_entity
+      @ride = current_user.rides.new(length: ride_params[:length], spots_left: ride_params[:spots_left], departure_date_time: ride_params[:departure_date_time], event: ride_params[:event], destination: destination, start_point: start_point)
+      @ride.save
+      end
+    rescue ActiveRecord::Invalid => invalid
+      p invalid.message
+      render text: invalid.message, status: 400
+    rescue Exception => e
+      puts e.message
+      render text: e.message, status: 400
+    else  
+      render json: @ride, status: :created
     end
   end
 
   def update
-    if @ride.update(ride_params)
+    ride = Ride.find_by_id(params[:id])
+    begin
+      Ride.transaction do
+        if ride_params[:destination]
+          destination = location.find_or_create_by!(address: ride_params[:address], lat: ride_params[:destination][:lat], lng:ride_params[:destination][:lng])
+          ride.update!(destination: destination)
+        end
+        if ride_params[:start_point]
+           start_point = location.find_or_create_by!(address: ride_params[:address], lat: ride_params[:destination][:lat], lng:ride_params[:destination][:lng])
+          ride.update!(start_point: start_point) 
+        end
+        ride.update(length: ride_params[:length], spots_left: ride_params[:spots_left], departure_date_time: ride_params[:departure_date_time], event: ride_params[:event])
+        end     
+    rescue Exception => e
+      puts e.message
+      render text: e.message, status: 400
+    else      
       render json: @ride
-    else
-      render json: @book.errors, status: :unprocessable_entity
     end
+       
   end
 
   def destroy
+    @ride = Ride.find_by_id(params[:id])
     @ride.destroy
 
-    head :no_content
+    render json: {message: 'Ride deleleted.'}
   end
 
   def ride_params
-    params.require(:ride).permit(:owner, :length, :spots_left, :departure_date_time, :destination, :start_point, :event)
+    params.require(:ride).permit(:owner, :length, :spots_left, :departure_date_time, :destination, :start_point, :event, :address)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@
 class User < ActiveRecord::Base
   include Authentication
 
-  has_many :rides
+  has_many :rides, :class_name => 'Ride', :foreign_key => 'owner_id'
   has_many :ride_passengers, through: :rides
 end
